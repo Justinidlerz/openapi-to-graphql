e0a3b7481e71d92591d2c21dbde68395b2a1314b
Justinidlerz <zqc.sunny@gmail.com>
2022-09-22 17:56:06 +0800

    Fix imports

--- packages/openapi-to-graphql/src/schema_builder.ts
+++ packages/openapi-to-graphql/src/schema_builder.ts
@@ -36,7 +36,7 @@ import {
   GraphQLInputType,
   GraphQLInputFieldConfigMap
 } from 'graphql'
-import GraphQLUpload from 'graphql-upload/GraphQLUpload'
+import { GraphQLUpload } from 'graphql-upload'
 
 // Imports:
 import { GraphQLBigInt, GraphQLJSON } from 'graphql-scalars'
